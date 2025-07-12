596569a78dc61acd0c7a298702833d9d35a75b85
Julien May <julien.may@gmail.com>
2025-07-12 23:51:02 +0200

    Don't fail if grep has no matches
    grep exits with status code 1 when no matches are found.
    Since the script uses set -e, this would normally cause an immediate exit.
    
    This change ensures the script continues even if grep returns no matches by explicitly
    allowing a non-zero exit status in that case.

--- .github/scripts/next-version.sh
+++ .github/scripts/next-version.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 today=$(date +'%Y.%m.%d')
 
 # List all tags for today, sorted by revision
-tags=$(git tag -l | grep -E "$today.*" | sort -t. -k4,4n)
+tags=$(git tag -l | grep -E "$today.*" | sort -t. -k4,4n || true)
 
 # The chronver requires the format to be YYYY.MM.DD[.revision]
 if [ -z "$tags" ]; then
